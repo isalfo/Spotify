f28b4bb27a6ca296e3426a6fb39a5d35995a81c5
Gonzalo Alfonso <gonzaloalfonso@Gonzalos-MacBook-Pro.local>
2022-07-15 13:11:06 -0300

    add auth firsts steps and login view on webView

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "05e724859de74cfe9c143ba16275b3ef"
+        static let clientSecret = "174120657cf944c39f04d5fe5d95b9b5"
+    }
+    
     private init() { }
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.google.com"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirectID=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,17 @@ final class AuthManager {
     
     private var shouldReturnToken: Bool {
         return false
+    }
+    
+    private func exchangeCodeForToken(code: String, completion: @escaping ((Bool)-> Void)) {
+        //GET TOKEN
+    }
+    
+    private func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
