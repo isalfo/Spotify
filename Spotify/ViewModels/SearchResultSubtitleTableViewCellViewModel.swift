9b74be27048bd984195fe7bc050c9972a49bae03
Gonzalo Alfonso <gonzaloalfonso@Gonzalos-MacBook-Pro.local>
2022-08-08 18:13:34 -0300

    add custom cells and navigation on searchController

---
+++ Spotify/ViewModels/SearchResultSubtitleTableViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultSubtitleTableViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Gonzalo Alfonso on 08/08/2022.
+//
+
+import Foundation
+
+struct SearchResultSubtitleTableViewCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+}
